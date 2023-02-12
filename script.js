6fabeb4eb5a140b3eb478b0309266ef80864717e
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-12 20:04:36 +0200

    task 2

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let users = [
+// task 1
+
+const users = [
    'Mike',
    'Nikola',
    'Tom'
@@ -8,3 +10,20 @@ users[1]='Alex';
 console.log(users[1]);
 users.length = 1;
 console.log(users.length);
+
+// task 2
+
+const a = [5, 3, -4, 25, 32, -16, 6];
+const b = [21, -30, 9, 5, 12, -19, 5, 25];
+function compareArr(){
+   if (a.length < b.length){
+      alert("a < b");
+   }
+   else if (a.length > b.length){
+      alert("a > b");
+   }
+   else if (a.length = b.length){
+      alert("a = b");
+   }
+}
+compareArr();
