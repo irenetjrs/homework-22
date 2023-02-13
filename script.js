a7eb79627d0c2f921a7fb896b7cf023f44baadc9
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-13 14:39:34 +0200

    task5

--- script.js
+++ script.js
@@ -50,3 +50,20 @@ console.log(g);
 
 // task 5
 
+let users = [
+   {id: 1, age: 17},
+   {id: 2, age: 18},
+   {id: 3, age: 19},
+   {id: 4, age: 21},
+   {id: 5, age: 17},
+   {id: 6, age: 20},
+   {id: 7, age: 25}
+];
+function selectionAge(){
+const checkAge = users.filter(id => id.age > 18 && id.age < 21);
+
+for (user of checkAge){
+console.log(user.id);
+}
+}
+selectionAge();
